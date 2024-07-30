ccba094618a693e16e39750c9845d34747ce0a65
omkarsn151 <omkarsnimbalkar1501@gmail.com>
2024-07-30 13:57:45 +0530

    1st change

--- lib/resources/socket_client.dart
+++ lib/resources/socket_client.dart
@@ -5,7 +5,7 @@ class SocketClient {
   static SocketClient? _instance;
 
   SocketClient._internal() {
-    socket = IO.io('http://192.168.29.64:3000', <String, dynamic>{
+    socket = IO.io('http://ipaddress:3000', <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': false,
     });
